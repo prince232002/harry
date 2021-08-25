588d1aa7ab55b2e5ae6c7df12b106e1327794f43
prince232002 <prince2002gupta@gmail.com>
2021-08-25 19:53:52 +0530

    ff

--- p73funccaching.py
+++ p73funccaching.py
@@ -1,8 +1,11 @@
-
+"""function caching -- allows us to cache the return values of a function depending on the arguments 
+it can save time when I/O bound function is periodicly called with the same arguments 
+@lru_cache decorator gives you the abillity to cache  result of your function using least used strategy 
+"""
 import time
 from functools import lru_cache
 
-@lru_cache(maxsize=32)
+@lru_cache(maxsize=23)
 def some_work(n):
     #Some task taking n seconds
     time.sleep(n)
@@ -12,10 +15,10 @@ if __name__ == '__main__':
     print("Now running some work")
     some_work(3)
     some_work(1)
-    some_work(6)
-    some_work(2)
+    # some_work(6)
+    # some_work(2)
     print("Done... Calling again")
-    input()
-    some_work(3)
+    some_work(5)
+    # input()
     print("Called again")
 
